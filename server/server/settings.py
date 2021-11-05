cc259423c70e0ebe82264ce156356c842fbf016f
Ganesh <ganeshn944870@gmail.com>
2021-11-05 16:18:28 +0530

    update postgres connection

--- server/server/settings.py
+++ server/server/settings.py
@@ -83,27 +83,26 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'MLsetdb',
-        # 'HOST': 'pgdb',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'PORT': 5432,
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'HOST': 'pgdb',
+#         'NAME': 'MLsetdb',
 #         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
+#         'PASSWORD': 'root',
 #         'PORT': 5432,
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'HOST': 'pgdb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
