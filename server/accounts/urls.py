d144b8f22e923994ff011a898d86bbe4452160cc
Ganesh <ganeshn944870@gmail.com>
2021-11-05 22:01:30 +0530

    revert and remove user/<email>

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from accounts.views import user_api, user_auth
+from accounts.views import user_api, user_signup
 
 urlpatterns = [
-    path('user_auth/', user_auth.as_view()),
-    # path('user_auth/<email>', user_auth.as_view()),
     path('user/', user_api.as_view()),
-    # path('user/<email>', user_api.as_view()),
+    path('user_signup/', user_signup.as_view()),
 ]
--- server/mlsetapp/tests.py
+++ server/mlsetapp/tests.py
@@ -5,7 +5,7 @@ from rest_framework import status
 class User_signup_testing(APITestCase):
     def setUp(self):
         self.user = {"username": "test@gmail.com", "first_name": "test", "last_name": "test", "email": "test@gmail.com", "password": "test"}
-        self.usercreatedresp = self.client.post("/accounts/user_auth/", self.user, format='json')
+        self.usercreatedresp = self.client.post("/accounts/user_signup/", self.user, format='json')
         auth_token = self.usercreatedresp.data['access']
         self.hed = {'HTTP_AUTHORIZATION': 'Bearer ' + auth_token}
 
