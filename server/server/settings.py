6fecf7579774c208edd2ee0e8373465c06cc7803
Ganesh <ganeshn944870@gmail.com>
2021-11-09 19:42:22 +0530

    update docker db

--- server/server/settings.py
+++ server/server/settings.py
@@ -95,28 +95,28 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # }
 
 # Github postgres setup
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    }
-}
-
-# Docker postgres setup
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
 #         'NAME': 'postgres',
-#         'HOST': 'postgres',
 #         'USER': 'postgres',
 #         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
 #         'PORT': 5432,
 #     }
 # }
+
+# Docker postgres setup
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'HOST': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
