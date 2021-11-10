b21388e6481a2e2379f88a866867e9d33b82a433
Ganesh <ganeshn944870@gmail.com>
2021-11-10 11:20:42 +0530

    docker

---
+++ client/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+npm cypress run --headless --spec "cypress/integration/MLset/MLset_end_to_end_testing.js"
+
+npm run int-test
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,11 +3,10 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-flake8 --ignore=E501
-coverage run manage.py test > coverage_report.txt
-coverage report
-ls -l
-cat coverage_report.txt
 
-# gunicorn server.wsgi:application --bind 0.0.0.0:8000
+flake8 --ignore=E501
+coverage run --omit=*/venv/*,*/migrations/*,*/MLsetenv/*,*/manage.py/* manage.py test & coverage report
+
+
+gunicorn server.wsgi:application --bind 0.0.0.0:8000
 
