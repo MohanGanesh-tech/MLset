b98d725c8504b97b8c9f760f70b7c56a478509dd
Ganesh <ganeshn944870@gmail.com>
2021-11-10 11:06:13 +0530

    docker

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,8 +4,10 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 flake8 --ignore=E501
-coverage run manage.py test
+coverage run manage.py test > coverage_report.txt
 coverage report
+ls -l
+cat coverage_report.txt
 
-gunicorn server.wsgi:application --bind 0.0.0.0:8000
+# gunicorn server.wsgi:application --bind 0.0.0.0:8000
 
